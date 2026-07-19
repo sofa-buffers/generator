72cfbab0ee297bd2178006ced5b93292e1afa735
Andreas Steinbart <andreas.steinbart@web.de>
2026-07-19 09:37:40 +0000

    release: v0.19.0

--- cmd/sofabgen/main.go
+++ cmd/sofabgen/main.go
@@ -40,7 +40,7 @@ import (
 	_ "github.com/sofa-buffers/generator/generators/zig"
 )
 
-const version = "0.18.0"
+const version = "0.19.0"
 
 func main() {
 	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
